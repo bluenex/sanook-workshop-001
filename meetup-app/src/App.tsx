47810e0087a86526d2ad3913730b7b1d23782661
tulakann <tulakan.r@gmail.com>
2019-04-23 20:06:02 +0700

    use styled-components

--- meetup-app/src/App.tsx
+++ meetup-app/src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const logoRotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AnimatedLogo = styled.img`
+  animation: ${logoRotation} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const CommonLink = styled.a`
+  color: #61dafb;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Header>
+        <AnimatedLogo
+          src={logo}
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <CommonLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </CommonLink>
+      </Header>
+    </Container>
   );
 }
 
